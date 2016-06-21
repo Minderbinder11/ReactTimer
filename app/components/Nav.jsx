ae2210ed67ed10743a460268e45e5793d4fcc6a0
Phil Barow <pbarow@gmail.com>
2016-06-21 13:37:36 -0700

    updating scss formatting

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,21 +7,21 @@ return (
   <div className="top-bar">
     <div className="top-bar-left">
       <ul className="menu">
-        <li className="menu-text">React Timer App
+        <li className="menu-text">
+          React Timer App
         </li>
         <li>
-          <IndexLink to="/" activeClassName="active-class">Timer</IndexLink>
+          <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
         </li>
         <li>
-          <Link to="/countdown" activeClassName="active-class">Countdown</Link>
+          <Link to="/" activeClassName="active-link">Countdown</Link>
         </li>
       </ul>
     </div>
     <div className="top-bar-right">
       <ul className="menu">
-        <li className="menu-text">Github Repository</li>
-        <li>
-          <a href="https://github.com/Minderbinder11" target="_blank">Minderbinder11</a>
+        <li className="menu-text">
+          Created by <a href="http://www.mead.io" target="_blank">Andrew Mead</a>
         </li>
       </ul>
     </div>
